TPReal/tp-vector

commit fc7bc1a200f819c8d23556e77719001c7d2a15fb

TabbedFace: The [0,0] point is now always the start of the path on base level, regardless of the actual start level.

src/tabbed_face.ts
@@ -628,6 +628,8 @@ export class TabbedFace<P extends string = never>
       let prevHopSegm: HopSegment | undefined;
       let nextHopSegm: HopSegment | undefined = this.segments.find(isHopSegment);
       let pointLevel = getPointLevel(prevHopSegm?.end, nextHopSegm?.start);
+      if (pointLevel === Level.TAB)
+        t = t.withPenUp(this.strafeToTab());
       for (let i = 0; i < this.segments.length; i++) {
         const segment = this.segments[i];
         if (segment.kind === "hop") {
@@ -639,6 +641,8 @@ export class TabbedFace<P extends string = never>
         } else
           t = t.andThen(segment.getFunc(pointLevel));
       }
+      if (pointLevel === Level.TAB)
+        t = t.withPenUp(this.strafeToTab(false));
       return t;
     };
   }

commit cda98516483037c3a5591e88eb3705cdc149bc4a

TabbedFace: Fixed a bug introduced by fc7bc1a that caused incorrect behaviour when TabbedFace was used as a TurtleFunc and not as a Piece.

src/tabbed_face.ts
@@ -626,17 +626,12 @@ export class TabbedFace<P extends string = never>
     return ClosedFace.create(path, this.tabsDict);
   }
 
-  /**
-   * TabbedFace can be used directly as a TurtleFunc.
-   * Note that the rotation is ignored in that case.
-   */
-  getFunc(): TurtleFunc {
-    return t => {
+  private collectFunctions() {
       let prevHopSegm: HopSegment | undefined;
       let nextHopSegm: HopSegment | undefined = this.segments.find(isHopSegment);
-      let pointLevel = getPointLevel(prevHopSegm?.end, nextHopSegm?.start);
-      if (pointLevel === Level.TAB)
-        t = t.withPenUp(this.strafeToTab());
+    const startLevel = getPointLevel(undefined, nextHopSegm?.start);
+    let pointLevel = startLevel;
+    const func: TurtleFunc = t => {
       for (let i = 0; i < this.segments.length; i++) {
         const segment = this.segments[i];
         if (segment.kind === "hop") {
@@ -648,14 +643,36 @@ export class TabbedFace<P extends string = never>
         } else
           t = t.andThen(segment.getFunc(pointLevel));
       }
-      if (pointLevel === Level.TAB)
-        t = t.withPenUp(this.strafeToTab(false));
       return t;
+    }
+    return {
+      func,
+      startLevel,
+      endLevel: pointLevel,
     };
   }
 
+  /**
+   * TabbedFace can be used directly as a TurtleFunc.
+   * Note that the start dir is ignored in that case.
+   */
+  getFunc(): TurtleFunc {
+    return this.collectFunctions().func;
+  }
+
+  /**
+   * Returns a Turtle that draws the tabbed face. The start direction of the turtle is
+   * as specified. The point `[0, 0]` is on the base level at the beginning of the path.
+   */
   asTurtle() {
-    return Turtle.create().setAngle(this.startDir ?? 0).andThen(this);
+    const {func, startLevel, endLevel} = this.collectFunctions();
+    let t = Turtle.create().setAngle(this.startDir ?? 0);
+    if (startLevel === Level.TAB)
+      t = t.withPenUp(this.strafeToTab());
+    t = t.andThen(func);
+    if (endLevel === Level.TAB)
+      t = t.withPenUp(this.strafeToTab(false));
+    return t;
   }
 
   asPath() {