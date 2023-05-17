fe0ddc27e8e6e95786337094a6b45d63b03be402
TPReal <tpreal@gmail.com>
2023-05-17 20:07:40 +0200

    Added some more rounding in the generated file.

--- src/colors.ts
+++ src/colors.ts
@@ -1,5 +1,7 @@
+import {roundReasonably} from "./util.ts";
+
 /** Returns a grey color of the specified darkness, 0 is white, 1 is black. */
 export function darkness(darknessValue: number) {
-  const val = 255 * (1 - darknessValue);
+  const val = roundReasonably(255 * (1 - darknessValue));
   return `rgb(${val},${val},${val})`;
 }
